cc917bd5b4a4abc24c5c0436eca328b0ab5c7355
politifan <68607255+politifan@users.noreply.github.com>
2026-07-09 19:23:44 +0300

    Change server port from 3000 to 3002

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { createApp } from './app.js';
 
-const port = Number.parseInt(process.env.PORT ?? '3000', 10);
+const port = Number.parseInt(process.env.PORT ?? '3002', 10);
 const app = createApp();
 
 app.listen(port, () => {
